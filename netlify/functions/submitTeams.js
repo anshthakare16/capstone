bc384100370e0cedcaed64738ff534b3d09221bd
anshthakare16 <anshthakare542@gmail.com>
2025-04-10 19:13:13 +0530

    Update submitTeams.js

--- netlify/functions/submitTeams.js
+++ netlify/functions/submitTeams.js
@@ -1,9 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
-const writeFile = promisify(fs.writeFile);
-const appendFile = promisify(fs.appendFile);
-const exists = promisify(fs.exists);
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://thakareansh3:ansh1212@ac-r0zzg7a-shard-00-00.vq4kvnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
@@ -24,34 +21,20 @@ exports.handler = async (event) => {
       };
     }
 
-    if (members.length > 4 || mentors.length > 4 || ideas.length > 3) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          error: 'Maximum 4 members, 4 mentors and 3 ideas allowed',
-        }),
-      };
-    }
+    const client = new MongoClient(uri);
+    await client.connect();
+    const db = client.db('TeamRegistration');
+    const teams = db.collection('teams');
 
-    const filePath = path.resolve(__dirname, 'data', 'teams.csv');
-    const fileExists = await exists(filePath);
+    await teams.insertOne({
+      class: selectedClass,
+      members,
+      mentors,
+      ideas,
+      timestamp: new Date()
+    });
 
-    if (!fileExists) {
-      const header = 'Class,Member1,Member2,Member3,Member4,Mentor1,Mentor2,Mentor3,Mentor4,Idea1,Idea2,Idea3\n';
-      await writeFile(filePath, header);
-    }
-
-    const row = [
-      selectedClass,
-      ...members,
-      ...Array(4 - members.length).fill(''),
-      ...mentors,
-      ...Array(4 - mentors.length).fill(''),
-      ...ideas,
-      ...Array(3 - ideas.length).fill(''),
-    ].join(',');
-
-    await appendFile(filePath, row + '\n');
+    await client.close();
 
     return {
       statusCode: 200,
